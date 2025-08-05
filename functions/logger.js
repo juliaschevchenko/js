d406bf2c9991ae9a14495abf2ddad04c96b6efc7
schevcha <j.shevcha@gmail.com>
2025-08-06 00:57:57 +0300

    upd logger

--- functions/logger.js
+++ functions/logger.js
@@ -1,10 +1,18 @@
-function logUserInfo(user, logger) {
-  const msg = `${user.name}, ${user.age} лет`;
-  logger(msg); // внешняя функция
-}
+const fs = require("fs");
+const path = require("path");
+
+const logFilePath = path.join(__dirname, "../logs/app.log");
 
 function log(message) {
-  console.log("LOG:", message);
+  const timestamp = new Date().toISOString();
+  const fullMessage = `[${timestamp}] ${message}\n`;
+
+  fs.appendFileSync(logFilePath, fullMessage, "utf8");
+}
+
+function logUserInfo(user, logger = log) {
+  const msg = `${user.name}, ${user.age} лет`;
+  logger(msg);
 }
 
 module.exports = { log, logUserInfo };
